8e9829c5e5d5315406758f3016ff8bad65f3f390
PalakPC <no.1palak@gmail.com>
2014-08-21 18:55:47 +0530

    Nodejs config.js removed

--- ifprint/ifprint.pl
+++ ifprint/ifprint.pl
@@ -1,14 +1,16 @@
 #!/usr/bin/perl
 
+#Perl script to print the instances of all ints and floats of code written in a file to another file along with line number.
+
 use strict;
 use warnings;
 use Getopt::Long;
 
-my ($input, $output);
+my($input, $output);
 my $v = '';
 my $f = '';
 
-usage () if (@ARGV < 1 or !GetOptions('v' => \$v, 'f' => \$f, 'i=s' => \$input, 'o=s' => \$output));
+usage() if(@ARGV < 1 or !GetOptions('v' => \$v, 'f' => \$f, 'i=s' => \$input, 'o=s' => \$output));
 
 sub usage {
 	print "Unknown option: @_\n" if (@_);
@@ -19,21 +21,21 @@ sub usage {
 my $regexv = qr|\b((int\|float)\s[A-Z, a-z, _]\w*(?!\())\b|;
 my $regexf = qr|\b((int\|float)\s[A-Z, a-z, _]\w*)\b|;
 
-open (FILE, "<$input");
+open(FILE, "<$input");
 
-open (OUT, ">$output");
+open(OUT, ">$output");
 
-if ($v eq '1' && $f eq '') {
-	while (<FILE>) {
-		while (/$regexv/g) {
+if($v eq '1' && $f eq '') {
+	while(<FILE>) {
+		while(/$regexv/g) {
 			print OUT "$1\t$.\n";
 		}
 	}
 }
 
-elsif ($v eq '' && $f==1) {
-	while (<FILE>) {
-		while (/$regexf/g) {
+elsif($v eq '' && $f==1) {
+	while(<FILE>) {
+		while(/$regexf/g) {
 			print OUT "$1\t$.\n";
 		}
 	}
