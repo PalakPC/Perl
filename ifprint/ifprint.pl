0106e7fb76cdaa5b221b01b5a691c2f1f662b372
PalakPC <no.1palak@gmail.com>
2014-08-31 20:34:35 +0530

    merge conflict resolved

--- ifprint/ifprint.pl
+++ ifprint/ifprint.pl
@@ -10,12 +10,13 @@ my($input, $output);
 my $v = '';
 my $f = '';
 
-usage() if(@ARGV < 1 or !GetOptions('v' => \$v, 'f' => \$f, 'i=s' => \$input, 'o=s' => \$output));
+usage() 
+   if(@ARGV < 1 or !GetOptions('v' => \$v, 'f' => \$f, 'i=s' => \$input, 'o=s' => \$output));
 
 sub usage {
-	print "Unknown option: @_\n" if (@_);
-	print "usage: program [--i inputfile] [--o outputfile]\n";
-	exit;
+   print "Unknown option: @_\n" if (@_);
+   print "usage: program [--v or --f] [--i inputfile] [--o outputfile]\n";
+   exit;
 }
 
 my $regexv = qr|\b((int\|float)\s[A-Z, a-z, _]\w*(?!\())\b|;
@@ -26,21 +27,21 @@ open(FILE, "<$input");
 open(OUT, ">$output");
 
 if($v eq '1' && $f eq '') {
-	while(<FILE>) {
-		while(/$regexv/g) {
-			print OUT "$1\t$.\n";
-		}
-	}
+   while(<FILE>) {
+      while(/$regexv/g) {
+         print OUT "$1\t$.\n";
+      }
+   }
 }
 
-elsif($v eq '' && $f==1) {
-	while(<FILE>) {
-		while(/$regexf/g) {
-			print OUT "$1\t$.\n";
-		}
-	}
+elsif($v eq '' && $f eq 1) {
+   while(<FILE>) {
+      while(/$regexf/g) {
+         print OUT "$1\t$.\n";
+      }
+   }
 }
 
 else {
-	print "Invalid arguments\n";
+   print "Invalid arguments\n";
 }
